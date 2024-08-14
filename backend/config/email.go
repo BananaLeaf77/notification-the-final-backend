37aedcb07862d2175c65c448df89bdecf595d70a
Ary_s <dognub61@gmail.com>
2024-08-14 18:43:44 +0800

    hhhbhb

---
+++ backend/config/email.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"gopkg.in/gomail.v2"
+)
+
+func sendEmail() {
+	m := gomail.NewMessage()
+	m.SetHeader("From", "madegedearysutha@email.com")
+	m.SetHeader("To", "dognub61@gmail.com")
+	m.SetHeader("Subject", "Hello Nigga!")
+	m.SetBody("text/plain", "Pada suatu hari saya pun me ngetest library gomail.")
+
+	d := gomail.NewDialer("mail.smtp2go.com", 2525, "your-username", "your-password")
+
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	sendEmail()
+}
--- backend/go.mod
+++ backend/go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/jackc/pgx/v5 v5.6.0
 	github.com/joho/godotenv v1.5.1
 	github.com/sirupsen/logrus v1.9.3
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
 
 require (
@@ -29,4 +30,5 @@ require (
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 )
