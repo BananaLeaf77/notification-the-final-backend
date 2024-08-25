7c3c97ee40c4e566a83b046d8477bf015436cdcf
Ary_s <dognub61@gmail.com>
2024-08-25 19:00:28 +0800

    email

--- backend/config/email.go
+++ backend/config/email.go
@@ -8,7 +8,7 @@ func sendEmail() {
 	m := gomail.NewMessage()
 	m.SetHeader("From", "madegedearysutha@email.com")
 	m.SetHeader("To", "dognub61@gmail.com")
-	m.SetHeader("Subject", "Hello Nigga!")
+	m.SetHeader("Subject", "Hello brodi!")
 	m.SetBody("text/plain", "Pada suatu hari saya pun me ngetest library gomail.")
 
 	d := gomail.NewDialer("mail.smtp2go.com", 2525, "your-username", "your-password")
