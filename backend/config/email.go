db59f05f07d741681b21cc2e34eeacaa691b6013
Ary_s <dognub61@gmail.com>
2024-08-28 20:15:10 +0800

    email

--- backend/config/email.go
+++ backend/config/email.go
@@ -1,23 +1,96 @@
 package config
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
 	"gopkg.in/gomail.v2"
 )
 
-func sendEmail() {
-	m := gomail.NewMessage()
-	m.SetHeader("From", "madegedearysutha@email.com")
-	m.SetHeader("To", "dognub61@gmail.com")
-	m.SetHeader("Subject", "Hello brodi!")
-	m.SetBody("text/plain", "Pada suatu hari saya pun me ngetest library gomail.")
+var goMailDialer *gomail.Dialer
 
-	d := gomail.NewDialer("mail.smtp2go.com", 2525, "your-username", "your-password")
+func InitEmailer() error {
+	goMailDialer = gomail.NewDialer("mail.smtp2go.com", 2525, "your-username", "your-password")
 
-	if err := d.DialAndSend(m); err != nil {
-		panic(err)
+	_, err := getSender()
+	if err != nil {
+		return err
 	}
+
+	_, err = getSchoolPhoneNumber()
+	if err != nil {
+		return err
+	}
+
+	return nil
+
 }
 
-func main() {
-	sendEmail()
+func SendEmail(studName string, parentName, emailAddress string) error {
+	goMailMessage := gomail.NewMessage()
+	schoolPhone, _ := getSchoolPhoneNumber()
+	senderEmail, err := getSender()
+	if err != nil {
+		return err
+	}
+	goMailMessage.SetHeader("From", senderEmail)
+
+	goMailMessage.SetHeader("To", emailAddress)
+
+	tNow := time.Now()
+	formattedDate := tNow.Format("02/01/2006")
+	hourOnly := tNow.Format("15")
+	hourAndMinute := tNow.Format("15:04")
+
+	intHourOnly, err := strconv.Atoi(hourOnly)
+	if err != nil {
+		return err
+	}
+	isAM := "AM"
+	if intHourOnly >= 12 {
+		isAM = "PM"
+	}
+
+	// email subject
+	subject := fmt.Sprintf("Pemberitahuan Ketidakhadiran %s pada %s %s, tanggal %s", studName, hourAndMinute, isAM, formattedDate)
+	goMailMessage.SetHeader("Subject", subject)
+
+	// email body
+	body := fmt.Sprintf(`Kepada Yth. Bapak/Ibu %s,
+
+Kami ingin memberitahukan bahwa putra/putri Bapak/Ibu, %s, tidak hadir di sekolah pada tanggal %s pukul %s %s.
+
+Alasan ketidakhadiran belum kami terima hingga saat ini. Kami berharap Bapak/Ibu dapat memberikan konfirmasi atau informasi lebih lanjut mengenai kondisi putra/putri Bapak/Ibu.
+
+Jika terdapat pertanyaan atau memerlukan bantuan lebih lanjut, Bapak/Ibu dapat menghubungi kami di %s.
+
+Terima kasih atas perhatian dan kerjasamanya.`, parentName, studName, formattedDate, hourAndMinute, isAM, schoolPhone)
+
+	goMailMessage.SetBody("text/plain", body)
+
+	// Send the email
+	if err := goMailDialer.DialAndSend(goMailMessage); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getSender() (string, error) {
+	emailSender := os.Getenv("EMAIL_SENDER")
+	if emailSender == "" {
+		return "", fmt.Errorf("empty email sender")
+	}
+	return emailSender, nil
+}
+
+func getSchoolPhoneNumber() (string, error) {
+	sp := os.Getenv("SCHOOL_PHONE")
+	if sp == "" {
+		return "", fmt.Errorf("empty school phone number")
+	}
+	return sp, nil
+
 }
